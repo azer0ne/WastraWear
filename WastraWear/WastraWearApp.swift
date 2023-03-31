bb7f9386c742eb32800e7ce719eb72d8801a1624
Abiyyu Reza <abiyyu.reza52@gmail.com>
2023-03-31 08:10:18 +0700

    Initial Commit

---
+++ WastraWear/WastraWearApp.swift
@@ -0,0 +1,17 @@
+//
+//  WastraWearApp.swift
+//  WastraWear
+//
+//  Created by Abiyyu Firmansyah on 31/03/23.
+//
+
+import SwiftUI
+
+@main
+struct WastraWearApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
